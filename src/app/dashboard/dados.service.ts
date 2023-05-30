aecff7ef1aad51d949548232b3cca0959bceb10f
JunioBatista <juniorjob21@gmail.com>
2023-05-30 01:20:41 -0300

    base chart

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable } from 'rxjs';
+@Injectable()
 export class DadosService {
 
-  constructor() { }
+    readonly dados = [
+        ['Janeiro', 33],
+        ['Fevereiro', 68],
+        ['Março', 49],
+        ['Abril', 15],
+        ['Maio', 80],
+        ['Junho', 27]
+    ];
+
+	constructor() {}
+
+	/**
+	 * Retorna um observable contendo os dados a serem
+	 * exibidos no gráfico.
+	 *
+	 * @return Observable<any>
+	 */
+	obterDados(): Observable<any> {
+		return new Observable(observable => {
+			observable.next(this.dados);
+			observable.complete();
+		});
+	}
+
 }
